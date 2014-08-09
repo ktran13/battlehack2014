efd009528dd2ace19de8a4adebedd5fd25f2dece
ktran13 <kristiantran@gmail.com>
2014-08-09 18:08:35 -0400

    updated changes

--- battlehackApp/app/scripts/features/discover/discoverCtrl.js
+++ battlehackApp/app/scripts/features/discover/discoverCtrl.js
@@ -10,26 +10,17 @@
  */
   var controllerId = 'DiscoverCtrl';
   var discoverCtrl = function ($log, $scope, dataService, paypalService, sharethisService) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
 
     var discoverCtrl = this;
-
-    discoverCtrl.friends = [{name:'John', phone:'555-1276'},
-                         {name:'Mary', phone:'800-BIG-MARY'},
-                         {name:'Mike', phone:'555-4321'},
-                         {name:'Adam', phone:'555-5678'},
-                         {name:'Julie', phone:'555-8765'},
-                         {name:'Juliette', phone:'555-5678'}];
 
     $log.debug(paypalService.test());
     $log.debug(sharethisService.test());
 
-    dataService.getDatabase().then(function() {
+    dataService.getDatabase().then(function(data) {
+      discoverCtrl.nonprofits = data.results;
+      $log.debug(data.results[0]);
       $log.debug('success!');
+
     }, function(error) {
       $log.debug('error!');
       $log.debug(error);
