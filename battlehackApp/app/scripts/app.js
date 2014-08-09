8adf288eb2e730811be5e4b803d9f16b640392e0
ktran13 <kristiantran@gmail.com>
2014-08-09 13:08:47 -0400

    generated angularjs front end app

---
+++ battlehackApp/app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name battlehackAppApp
+ * @description
+ * # battlehackAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('battlehackAppApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ battlehackApp/app/scripts/features/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name battlehackAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the battlehackAppApp
+ */
+angular.module('battlehackAppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
